9e394d2ed0ffa39433ddddbfc4f83e7bf59fbeb8
0mish <47588077+0mish@users.noreply.github.com>
2023-08-27 17:24:42 +0400

    Clean up benchmarks

--- tests/Evdb.Benchmarks/Program.cs
+++ tests/Evdb.Benchmarks/Program.cs
@@ -3,130 +3,107 @@ using Evdb.IO;
 using System.Diagnostics;
 using System.Text;
 
-SingleWriterSingleReader(iter: 10, entries: 1000);
-MultipleWritersSingleReader(iter: 10, entries: 1000);
-
-static void MultipleWritersSingleReader(int iter, int entries)
+Func<BenchmarkResult>[] benchmarks = new Func<BenchmarkResult>[]
 {
-    Console.WriteLine($"{nameof(MultipleWritersSingleReader)}:");
+    () => SingleWriterSingleReader(entries: 10000),
+    () => MultipleWritersSingleReader(entries: 10000)
+};
 
-    ulong tmiss = 0;
-    TimeSpan awts = default;
-    TimeSpan arts = default;
+string header = $"{"Benchmark Name",50} | {"Bytes Written/s",18:f2} | {"Bytes Read/s",18:f2}";
+string divider = new('-', header.Length);
 
-    Dictionary<string, byte[]> kvs = GenerateKeyValues(entries);
+Console.WriteLine(header);
+Console.WriteLine(divider);
 
-    for (int j = 0; j < iter; j++)
-    {
-        LsmIndexOptions options = new()
-        {
-            Path = "db"
-        };
+foreach (Func<BenchmarkResult> benchmark in benchmarks)
+{
+    BenchmarkResult result = benchmark();
 
-        using LsmIndex db = new(options);
+    double bytesWrittenPerSecond = result.BytesWritten / result.Duration.TotalSeconds;
+    double bytesReadPerSecond = result.BytesRead / result.Duration.TotalSeconds;
 
-        Stopwatch sw = Stopwatch.StartNew();
-
-        // TODO: Use a fixed number of threads here.
-        Parallel.ForEach(kvs, kv =>
-        {
-            db.TrySet(kv.Key, kv.Value);
-        });
-
-        ulong miss = 0;
-        TimeSpan wts = sw.Elapsed;
-
-        foreach (var kv in kvs)
-        {
-            if (!db.TryGet(kv.Key, out ReadOnlySpan<byte> val) || !val.SequenceEqual(kv.Value))
-            {
-                miss++;
-            }
-        }
-
-        TimeSpan rts = sw.Elapsed - wts;
-
-        Console.WriteLine($"Done in {(rts + wts).TotalMilliseconds}ms #{j}");
-        Console.WriteLine($" w {wts.TotalMilliseconds} ms {entries / wts.TotalMilliseconds:f2} p/s");
-        Console.WriteLine($" r {rts.TotalMilliseconds} ms {entries / rts.TotalMilliseconds:f2} g/s {miss} misses");
-
-        awts += wts;
-        arts += rts;
-        tmiss += miss;
-    }
-
-    Console.WriteLine();
-    Console.WriteLine($"Done in {(arts + awts).TotalMilliseconds}ms <>");
-    Console.WriteLine($" w {awts.TotalMilliseconds} ms {iter * entries / awts.TotalMilliseconds:f2} p/s");
-    Console.WriteLine($" r {arts.TotalMilliseconds} ms {iter * entries / arts.TotalMilliseconds:f2} g/s {tmiss} misses");
-    Console.WriteLine();
-    Console.WriteLine();
+    Console.WriteLine($"{result.Name,50} | {bytesWrittenPerSecond,10:f2} bytes/s | {bytesReadPerSecond,10:f2} bytes/s");
 }
 
-static void SingleWriterSingleReader(int iter, int entries)
+static BenchmarkResult MultipleWritersSingleReader(int entries)
 {
-    Console.WriteLine($"{nameof(SingleWriterSingleReader)}:");
-
-    ulong tmiss = 0;
-    TimeSpan awts = default;
-    TimeSpan arts = default;
-
-    Dictionary<string, byte[]> kvs = GenerateKeyValues(entries);
-
-    for (int j = 0; j < iter; j++)
+    Dictionary<string, byte[]> kvs = GenerateKeyValues(entries, keySize: 12, valueSize: 64);
+    LsmIndexOptions options = new()
     {
-        LsmIndexOptions options = new()
+        Path = "db"
+    };
+
+    using LsmIndex db = new(options);
+
+    Stopwatch sw = Stopwatch.StartNew();
+
+    // TODO: Use a fixed number of threads here.
+    Parallel.ForEach(kvs, kv =>
+    {
+        db.TrySet(kv.Key, kv.Value);
+    });
+
+    ulong miss = 0;
+    TimeSpan wts = sw.Elapsed;
+
+    foreach (var kv in kvs)
+    {
+        if (!db.TryGet(kv.Key, out ReadOnlySpan<byte> val) || !val.SequenceEqual(kv.Value))
         {
-            Path = "db",
-            FileSystem = new FileSystem()
-        };
-
-        using LsmIndex db = new(options);
-
-        Stopwatch sw = Stopwatch.StartNew();
-
-        foreach (var kv in kvs)
-        {
-            db.TrySet(kv.Key, kv.Value);
+            miss++;
         }
-
-        ulong miss = 0;
-        TimeSpan wts = sw.Elapsed;
-
-        foreach (var kv in kvs)
-        {
-            if (!db.TryGet(kv.Key, out ReadOnlySpan<byte> val) || !val.SequenceEqual(kv.Value))
-            {
-                miss++;
-            }
-        }
-
-        TimeSpan rts = sw.Elapsed - wts;
-
-        Console.WriteLine($"Done in {(rts + wts).TotalMilliseconds}ms #{j}");
-        Console.WriteLine($" w {wts.TotalMilliseconds} ms {entries / wts.TotalMilliseconds:f2} p/s");
-        Console.WriteLine($" r {rts.TotalMilliseconds} ms {entries / rts.TotalMilliseconds:f2} g/s {miss} misses");
-
-        awts += wts;
-        arts += rts;
-        tmiss += miss;
     }
 
-    Console.WriteLine();
-    Console.WriteLine($"Done in {(arts + awts).TotalMilliseconds}ms <>");
-    Console.WriteLine($" w {awts.TotalMilliseconds} ms {iter * entries / awts.TotalMilliseconds:f2} p/s");
-    Console.WriteLine($" r {arts.TotalMilliseconds} ms {iter * entries / arts.TotalMilliseconds:f2} g/s {tmiss} misses");
-    Console.WriteLine();
-    Console.WriteLine();
+    // TODO: Actually calculate metric from the index.
+    TimeSpan rts = sw.Elapsed - wts;
+    ulong readWrite = (ulong)entries * (12 + 64);
+
+    return new BenchmarkResult("Multiple Writers then Single Reader", readWrite, readWrite, wts + rts);
 }
 
-static Dictionary<string, byte[]> GenerateKeyValues(int count)
+static BenchmarkResult SingleWriterSingleReader(int entries)
+{
+    Dictionary<string, byte[]> kvs = GenerateKeyValues(entries, keySize: 12, valueSize: 64);
+    LsmIndexOptions options = new()
+    {
+        Path = "db",
+        FileSystem = new FileSystem()
+    };
+
+    using LsmIndex db = new(options);
+
+    Stopwatch sw = Stopwatch.StartNew();
+
+    foreach (var kv in kvs)
+    {
+        db.TrySet(kv.Key, kv.Value);
+    }
+
+    ulong miss = 0;
+    TimeSpan wts = sw.Elapsed;
+
+    foreach (var kv in kvs)
+    {
+        if (!db.TryGet(kv.Key, out ReadOnlySpan<byte> val) || !val.SequenceEqual(kv.Value))
+        {
+            miss++;
+        }
+    }
+
+    // TODO: Actually calculate metric from the index.
+    TimeSpan rts = sw.Elapsed - wts;
+    ulong readWrite = (ulong)entries * (12 + 64);
+
+    return new BenchmarkResult("Single Writer then Single Reader", readWrite, readWrite, wts + rts);
+}
+
+static Dictionary<string, byte[]> GenerateKeyValues(int count, int keySize, int valueSize)
 {
     Random random = new(Seed: 0);
     Dictionary<string, byte[]> kvs = new();
 
-    byte[] key = new byte[12];
-    byte[] value = new byte[64];
+    byte[] key = new byte[keySize];
+    byte[] value = new byte[valueSize];
 
     for (int i = 0; i < count; i++)
     {
@@ -138,3 +115,5 @@ static Dictionary<string, byte[]> GenerateKeyValues(int count)
 
     return kvs;
 }
+
+record class BenchmarkResult(string Name, ulong BytesWritten, ulong BytesRead, TimeSpan Duration);
