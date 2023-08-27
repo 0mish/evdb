6cd5ae2e9a53a9e4a090a6f9a750bebfa018ba97
0mish <47588077+0mish@users.noreply.github.com>
2023-08-27 16:48:20 +0400

    Add initial `Store` class
    Where the good stuff will happen.

---
+++ src/Evdb/Store.cs
@@ -0,0 +1,19 @@
+﻿namespace Evdb;
+
+public class Store
+{
+    public Store(string path)
+    {
+
+    }
+
+    public void Write(string name, in ReadOnlySpan<byte> value)
+    {
+
+    }
+
+    public void Read(string name, out ReadOnlySpan<byte> value)
+    {
+        value = default;
+    }
+}
